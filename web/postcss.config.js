c207d29cc8ace4cc507dabbdcb688e05f8486bef
Benson Trent <bensontrent@gmail.com>
2019-02-05 23:05:12 -0800

    Fix Font Loading, Add Open Sans, and Heading font

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -3,6 +3,21 @@ module.exports = () => ({
     require('postcss-import'),
     require('postcss-preset-env')({
       stage: 0
+    }),
+    require('postcss-font-magician')({
+      variants: {
+        'Montserrat': {
+          '300': [],
+          '400': [],
+          '700': []
+        },
+        'Open+Sans': {
+          '300': [],
+          '400': [],
+          '700': []
+        }
+      },
+      foundries: ['google']
     })
   ]
 })
