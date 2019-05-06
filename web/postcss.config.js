f16fa169bea15b2d50c8e11ee0428c83ce13bd2f
Benson Trent <bensontrent@gmail.com>
2019-05-06 11:39:02 -0700

    remove font magician

--- web/postcss.config.js
+++ web/postcss.config.js
@@ -3,21 +3,6 @@ module.exports = () => ({
     require('postcss-import'),
     require('postcss-preset-env')({
       stage: 0
-    }),
-    require('postcss-font-magician')({
-      variants: {
-        'Montserrat': {
-          '300': [],
-          '400': [],
-          '700': []
-        },
-        'Open+Sans': {
-          '300': [],
-          '400': [],
-          '700': []
-        }
-      },
-      foundries: ['google']
     })
   ]
 })
