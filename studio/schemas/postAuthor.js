47acbff3379551e31f5cddbf0da11c72a601def6
Benson Trent <bensontrent@gmail.com>
2020-03-04 04:14:52 -0800

    Add blog roles

--- studio/schemas/postAuthor.js
+++ studio/schemas/postAuthor.js
@@ -1,42 +1,50 @@
 export default {
-  type: 'object',
-  name: 'postAuthor',
-  title: 'Post author',
+  type: "object",
+  name: "postAuthor",
+  title: "Post author",
   fields: [
     {
-      title: 'Person',
-      name: 'person',
-      type: 'reference',
-      to: { type: 'person' }
+      title: "Person",
+      name: "person",
+      type: "reference",
+      to: { type: "person" }
     },
     {
-      title: 'Roles',
-      name: 'roles',
-      type: 'array',
-      of: [{ type: 'string' }],
+      title: "Roles",
+      name: "roles",
+      type: "array",
+      of: [{ type: "string" }],
       options: {
-        layout: 'radio',
+        layout: "radio",
         list: [
-          { title: 'Illustration', value: 'illustration' },
-          { title: 'Text', value: 'text' },
-          { title: 'Editor', value: 'editor' }
+          { title: "Designer", value: "designer" },
+          { title: "Creator", value: "creator" },
+          { title: "Developer", value: "developer" },
+          { title: "Illustrator", value: "illustration" },
+          { title: "Text", value: "text" },
+          { title: "Editor", value: "editor" },
+          { title: "Music-to-light Choreographer", value: "choreographer" },
+          { title: "Programmer", value: "programmer" },
+          { title: "Manager", value: "manager" },
+          { title: "Photographer", value: "photographer" },
+          { title: "Copywriter", value: "copywriter" }
         ]
       }
     }
   ],
   preview: {
     select: {
-      personName: 'person.name',
-      roles: 'roles',
-      image: 'person.image'
+      personName: "person.name",
+      roles: "roles",
+      image: "person.image"
     },
-    prepare (data) {
+    prepare(data) {
       return {
         ...data,
         title: data.personName,
-        subtitle: data.roles && data.roles.join('/'),
+        subtitle: data.roles && data.roles.join("/"),
         media: data.image
-      }
+      };
     }
   }
-}
+};
